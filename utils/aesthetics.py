048c1f297c6fc18c2f24920ec9b2a905457d8e66
Rudy Huezo <rahuezo@ucdavis.edu>
2019-01-03 05:02:29 -0800

    Removed prints in aesthetics.py

--- utils/aesthetics.py
+++ utils/aesthetics.py
@@ -1,10 +1,9 @@
 def fix_rows(rows): 
     print "\t\nFixing rows"
     for i in xrange(len(rows)):
-        print "Current row: ", rows[i]
-        if len(rows[i]) > 0 and ("\n" in str(rows[i]) or "\r" in str(rows[i])):
+        # print "Current row: ", rows[i]
+        if len(rows[i]) > 0: # and ("\n" in str(rows[i]) or "\r" in str(rows[i])):
             # Fix first column
-            
             if isinstance(rows[i][0], basestring):
                 rows[i][0] = rows[i][0].replace("\r", "\n")
                 rows[i][0] = rows[i][0].replace("\n", " ")
